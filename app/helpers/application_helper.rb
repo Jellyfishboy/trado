c71881b312597f732e0a7a374c75046cb2a2fdc5
Billy Dallimore <billy@sngtrkr.com>
2013-09-30 17:19:08 +0100

    Updated accessory page and setup bespoke helper for active page class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
       end
       link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
     end
+
+    def active_page?(page)
+      "active" if params[:controller] == page
+    end
 end
