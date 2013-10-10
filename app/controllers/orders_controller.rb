a56331c299f6482805d7c06fccb3bf04ce2caed0
Billy Dallimore <billy@sngtrkr.com>
2013-10-10 17:25:49 +0100

    Cleaning up order object and starting the production of the show order functionality in the admin interface

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -59,7 +59,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        Notifier.order_received(@order).deliver # pass the current @order into the method within Notifier class, then execute the delivery
+        #Notifier.order_received(@order).deliver # pass the current @order into the method within Notifier class, then execute the delivery
         format.html { redirect_to store_url, notice: 'Thank you for your order.' }
         format.json { render json: @order, status: :created, location: @order }
       else
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
-  attr_accessible :first_name, :last_name, :billing_company, :billing_address, :billing_city, :billing_county, :billing_postcode, :billing_country, :billing_telephone, :delivery_address, :delivery_city, :delivery_county, :delivery_postcode, :delivery_country, :delivery_telephone, :email, :tax_number, :total, :total_vat, :shipping_cost, :payment_status, :shipping_status, :shipping_date
+  attr_accessible :first_name, :last_name, :billing_company, :billing_address, :billing_city, :billing_county, :billing_postcode, :billing_country, :billing_telephone, :delivery_address, :delivery_city, :delivery_county, :delivery_postcode, :delivery_country, :delivery_telephone, :email, :tax_number, :total, :total_vat, :shipping_cost, :payment_status, :shipping_status, :shipping_date, :invoice_id, :actual_shipping_cost
   validates :first_name, :last_name, :email, :billing_address, :billing_city, :billing_county, :billing_postcode, :billing_country, :delivery_address, :delivery_city, :delivery_county, :delivery_postcode, :delivery_country, :presence => true
   validates_format_of :email, :with => /@/
   validates :terms, :acceptance => {:message => "Please accept the Terms & Conditions."}
-  after_update :send_new_ship_email, :if => :ship_date_changed? && :no_ship_date
-  after_update :send_changed_ship_email, :if => :ship_date_changed? && :ship_date_was
+  after_update :send_new_ship_email, :if => :shipping_date_changed? && :no_shipping_date
+  after_update :send_changed_ship_email, :if => :shipping_date_changed? && :shipping_date_was
 
   def add_line_items_from_cart(cart)
   	cart.line_items.each do |item|
@@ -14,8 +14,8 @@ class Order < ActiveRecord::Base
   	end
   end
 
-  def no_ship_date
-    self.ship_date_was.nil?
+  def no_shipping_date
+    self.shipping_date_was.nil?
   end
 
   def send_new_ship_email
