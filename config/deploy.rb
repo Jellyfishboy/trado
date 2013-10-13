40f131255259a714b8e4526234cde3e3c26cabe8
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-10-14 00:19:33 +0100

    Started capistrano deployment setup

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, 'gimson_robotics'
+set :user, 'root'
+set :scm, 'git'
+set :repository, 'git@github.com:Jellyfishboy/gimson_robotics.git'
+set :scm_verbose, true
+set :domain, '146.185.130.90'
+set :deploy_to, '/var/www/gimsonrobotics/'
+set :branch, 'master'
+
+server domain, :app, :web, :db, :primary => true
+
+# Bundler for remote gem installs
+require "bundler/capistrano"
+
+# Only keep the latest 3 releases
+set :keep_releases, 3
+after "deploy:restart", "deploy:cleanup"
+
+# deploy config
+set :deploy_via, :remote_cache
+set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
+set :use_sudo, false
+
+# additional settings
+default_run_options[:pty] = true
