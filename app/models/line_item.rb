6946f9ac30b9e824cff2458f1ae2ad934707f45e
Billy Dallimore <billy@sngtrkr.com>
2013-10-24 17:26:37 +0100

    Fixed several bugs for assigning dimension per line item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,6 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :price, :weight, :thickness, :length, :dimension_id
+  validates :dimension_id, :presence => true
   belongs_to :product #the parent of lineitems is product
   belongs_to :cart #the parent of lineitems is cart
   belongs_to :order
