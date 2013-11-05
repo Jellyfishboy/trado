4543070a72940c1162b4b1fdfb5ba5b225093635
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-05 18:16:17 +0000

    Fix for unicorn server

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.gimsonrobotics.sock"
+listen "/tmp/unicorn.projectname.sock"
 worker_processes 2
 timeout 30
 
