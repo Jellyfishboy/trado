7351466df814fb9f5d48aa4680dee5c656805618
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-05 15:48:15 +0000

    Changed database to mysql2

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.12'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 # Database
-gem 'sqlite3'
+gem 'mysql2'
 
 # HTML Framework
 gem 'haml'
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate_user!, :except => :new
-  load_and_authorize_resource
+  load_and_authorize_resource, :except => :new
 
     def show
         @user = User.find(params[:id])
