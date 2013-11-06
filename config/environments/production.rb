a07410fc4a63441c2c894a87663012569db63db5
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-06 21:55:37 +0000

    Added asset precompilation to the production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ GimsonRobotics::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile += %w(*/application.js */application.css)
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
