eebebf5804778e21bf6b149b2ff9a017ea538ab4
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-07 20:43:12 +0000

    Added asset setting to application config

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module GimsonRobotics
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
