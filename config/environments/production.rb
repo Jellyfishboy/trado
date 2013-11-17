32595702a262d867443cfbf9c4fcb48bed0cc4b5
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-17 19:31:34 +0000

    Updated production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,8 @@ GimsonRobotics::Application.configure do
   config.assets.compile = false
 
   config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
-  config.assets.precompile += %w(*/application.js */application.css */administration.css */administration.js)
+  config.assets.precompile += %w(*/application.js */application.css)
+  config.assets.precompile += %w(*/administration.css */administration.js)
 
   # Generate digests for assets URLs
   config.assets.digest = true
