dfb97fc04649545a76f388ed9eb3d637e2ae2275
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-17 19:17:44 +0000

    Changed compliation settings for application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ GimsonRobotics::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.assets.precompile += %w( *.js *.scss *.coffee *.css )
+  config.assets.precompile += %w( *.js *.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
