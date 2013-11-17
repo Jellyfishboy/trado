0179c7dd7385bf846546ab7148fb0edbdc4deac8
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-17 20:24:24 +0000

    Small modifications to the production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ GimsonRobotics::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.assets.precompile += ['administration.js', 'administration.css']
+  config.assets.precompile += ['administration.js', 'administration.css','rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Generate digests for assets URLs
   config.assets.digest = true
