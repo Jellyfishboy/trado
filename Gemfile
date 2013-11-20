57dd56e10880ffc5fed2a9a60abb246ea6e29a8a
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-20 23:15:11 +0000

    Started asset sync config

--- Gemfile
+++ Gemfile
@@ -46,10 +46,8 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'jquery-rails'
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
+  gem 'asset_sync'
 end
 
 platforms :ruby do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,10 @@ GimsonRobotics::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.action_controller.asset_host = "//gimson-robotics-production.s3.amazonaws.com"
+
+  config.assets.prefix = "/assets"
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
