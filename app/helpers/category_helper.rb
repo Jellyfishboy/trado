831aa792e8417bd51683a3ab24b3f7400bcf5963
JellyFishBoy <tom@ruckusmedia.co.uk>
2014-01-08 23:34:31 +0000

    Added price range and sorted the attribute values by ascending.

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -1,6 +1,12 @@
 module CategoryHelper
 
     def price_range product
-       
+        min = product.skus.minimum(:price)
+        max = product.skus.maximum(:price)
+        if min == max 
+            "#{format_currency(max)}".html_safe
+        else
+            "#{format_currency(min)} - #{format_currency(max)}".html_safe
+        end
     end
 end
