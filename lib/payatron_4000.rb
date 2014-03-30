fd481d148705933065232be803e2e4f3df61b44d
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-03-30 02:07:24 +0100

    Cleaning up the payatron_4000 library class.

--- lib/payatron_4000.rb
+++ lib/payatron_4000.rb
@@ -2,11 +2,11 @@ require 'payatron_4000/paypal'
 
 module Payatron4000
 
-    def self.price_in_pennies(price)
+    def self.price_in_pennies price
         (price*100).round
     end
 
-    def self.stock_update (order)
+    def self.stock_update order
         order.order_items.each do |item|
           sku = Sku.find(item.sku_id)
           sku.update_column(:stock, sku.stock-item.quantity)
