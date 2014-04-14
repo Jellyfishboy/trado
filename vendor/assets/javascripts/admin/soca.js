0f2527b87ff5ba12ca52a82a417012883af14af9
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-04-15 00:07:11 +0100

    Updated soca.js to the unmangled version ready for some AJAX bug fixing.

--- vendor/assets/javascripts/admin/soca.js
+++ vendor/assets/javascripts/admin/soca.js
@@ -1,1 +1,44 @@
-(function(){var attachment_ui;$(document).ready(function(){return $(".user-menu ul li:first-child a").hover(function(){return $(".user-menu .fa-caret-up").css("color","#2f363d")},function(){return $(".user-menu .fa-caret-up").css("color","#ffffff")}),attachment_ui(),$(".current-file").click(function(){return $(this).next('input[type="radio"]').prop("checked",!0),$(".current-file").removeClass("default"),$(this).addClass("default")})}),$(document).ajaxComplete(function(){return attachment_ui()}),attachment_ui=function(){return $(".new-file").on("click",function(){return $(this).next('input[type="file"]').trigger("click")}),$(".file-upload").change(function(){var clean,parent,value;return value=$(this).val(),clean=value.replace(/^.*[\\\/]/,""),parent=$(this).prev(".new-file"),parent.children("div").text(clean),clean?(parent.css("background-color","#8DC73F"),parent.children(".icon-upload-3").css("top","20px")):(parent.css("background-color","#00aff1"),parent.children(".icon-upload-3").css("top","41px"))})}}).call(this);
+(function() {
+  var attachment_ui;
+
+  $(document).ready(function() {
+    $(".user-menu ul li:first-child a").hover((function() {
+      return $(".user-menu .fa-caret-up").css('color', '#2f363d');
+    }), function() {
+      return $(".user-menu .fa-caret-up").css('color', '#ffffff');
+    });
+    attachment_ui();
+    return $('.current-file').click(function() {
+      $(this).next('input[type="radio"]').prop('checked', true);
+      $('.current-file').removeClass('default');
+      return $(this).addClass('default');
+    });
+  });
+
+  $(document).ajaxComplete(function() {
+    return attachment_ui();
+  });
+
+  attachment_ui = function() {
+    $('.new-file').on('click', function() {
+      return $(this).next('input[type="file"]').trigger('click');
+    });
+    return $('.file-upload').change(function() {
+      var clean, parent, value;
+      value = $(this).val();
+      clean = value.replace(/^.*[\\\/]/, '');
+      parent = $(this).prev('.new-file');
+      parent.children('div').text(clean);
+      if (clean) {
+        parent.css('background-color', '#8DC73F');
+        return parent.children('.icon-upload-3').css('top', '20px');
+      } else {
+        parent.css('background-color', '#00aff1');
+        return parent.children('.icon-upload-3').css('top', '41px');
+      }
+    });
+  };
+
+}).call(this);
+
+//# sourceMappingURL=application.js.map
