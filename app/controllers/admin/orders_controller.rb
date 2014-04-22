71b7caa2cd79d72c5954ef1a3f1c00735bbba2be
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-04-22 23:35:16 +0100

    Cleaning up modal DOM elements.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -22,7 +22,6 @@ class Admin::OrdersController < ApplicationController
   # PUT /orders/1.json
   def update
     @order = Order.find(params[:id])
-    binding.pry
     @order.shipping_date = DateTime.strptime(params[:order][:shipping_date], "%d/%m/%Y").to_time
     respond_to do |format|
       if @order.update_attributes(params[:order])
