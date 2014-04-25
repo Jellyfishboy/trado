e5ea459a20b6d13c76338c67ea6a6f716cec7ee1
Billy Dallimore <billy@sngtrkr.com>
2014-04-25 16:28:33 +0100

    Cleaned up the insufficient stock views and AJAX calls and started fixing the cart item insufficient stock business logic.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,15 +9,17 @@ class CartItemsController < ApplicationController
     @cart = current_cart #references the current cart which was defined in application controller
     accessory = params[:cart_item][:cart_item_accessory].nil? ? nil : Accessory.where('id = ?', params[:cart_item][:cart_item_accessory][:accessory_id]).first
     @cart_item = @cart.add_cart_item(sku, params[:cart_item][:quantity], accessory) #uses add_cart_item method in cart.rb to check if the cart item already exists in the cart and responds accordingly
+    # Need to query the database again since it will need to retrieve the quantity sum of cart_items with accessories and without accessories
+    @quantity = CartItem.where(sku_id: @cart_item.sku_id).sum(:quantity) + 1
     respond_to do |format|
-      if sku.stock >= @cart_item.quantity #checks to make sure the requested quantity is not more than the current DB stock
+      if sku.stock >= @quantity #checks to make sure the requested quantity is not more than the current DB stock
         if @cart_item.save
           format.js { render :partial => 'carts/update', :formats => [:js] }
         else
           format.json { render json: @cart_item.errors, status: :unprocessable_entity }
         end
       else
-        format.js { render :partial => 'carts/insufficient_stock', :formats => [:js] }
+        format.js { render :partial => 'carts/insufficient_stock/update', :formats => [:js] }
       end
     end
   end
@@ -41,7 +43,7 @@ class CartItemsController < ApplicationController
             format.json { render json: @category.errors, status: :unprocessable_entity }
           end
         else
-          format.js { render :partial => 'carts/insufficient_stock', :formats => [:js] }
+          format.js { render :partial => 'carts/insufficient_stock/update', :formats => [:js] }
         end
       end
   end
