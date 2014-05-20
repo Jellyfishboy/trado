1929355563f50774475ec888db5c34112923a19c
Billy Dallimore <billy@sngtrkr.com>
2014-05-20 13:07:03 +0100

    Refactored price_range method using a ternary operator.

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -3,10 +3,6 @@ module CategoryHelper
     def price_range product
         min = product.skus.minimum(:price)
         max = product.skus.maximum(:price)
-        if min == max 
-            "#{format_currency(max)}".html_safe
-        else
-            "#{format_currency(min)} - #{format_currency(max)}".html_safe
-        end
+        min == max ? "#{format_currency(max)}".html_safe : "#{format_currency(min)} - #{format_currency(max)}".html_safe
     end
 end
