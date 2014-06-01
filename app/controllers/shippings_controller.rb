9e5375614973124d389381ed9edba61c2f430249
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-06-02 00:57:49 +0100

    Ordered shipping results by price ascending.

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -8,7 +8,7 @@ class ShippingsController < ApplicationController
     def update
         @tiers = Shipatron4000::tier(current_cart)
         @shippings = @tiers.map{ |t| t.shippings.map{ |s| s } }.flatten
-        @new_shippings = Shipping.where(:id => @shippings.map(&:id)).joins(:countries).where('countries.name = ?', params[:country_id]).all
+        @new_shippings = Shipping.where(:id => @shippings.map(&:id)).joins(:countries).where('countries.name = ?', params[:country_id]).order('price ASC').all
         render :partial => "orders/shippings/update"
     end
 end
