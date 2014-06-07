68434cde556946de43d1241e4b7f0ce25f555c44
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-06-07 18:33:17 +0100

    Removed commenting.

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -3,91 +3,91 @@ require 'spec_helper'
 describe CartItem do
 
     # ActiveRecord relations
-    # it { expect(subject).to belong_to(:sku) }
-    # it { expect(subject).to belong_to(:cart) }
-    # it { expect(subject).to have_one(:cart_item_accessory).dependent(:delete) }
+    it { expect(subject).to belong_to(:sku) }
+    it { expect(subject).to belong_to(:cart) }
+    it { expect(subject).to have_one(:cart_item_accessory).dependent(:delete) }
 
-    # # Nested attributes
-    # it { expect(subject).to accept_nested_attributes_for(:cart_item_accessory) }
+    # Nested attributes
+    it { expect(subject).to accept_nested_attributes_for(:cart_item_accessory) }
 
-    # describe "When calculating a cart item total" do
-    #     let!(:cart_item) { build(:cart_item, price: 12, quantity: 7) }
-    #     it "should return the sum of price multiplied by quantity" do
-    #         expect(cart_item.total_price).to eq 84
-    #     end
-    # end
+    describe "When calculating a cart item total" do
+        let!(:cart_item) { build(:cart_item, price: 12, quantity: 7) }
+        it "should return the sum of price multiplied by quantity" do
+            expect(cart_item.total_price).to eq 84
+        end
+    end
 
-    # describe "When adding a new cart item" do
+    describe "When adding a new cart item" do
 
-    #     context "if its a new cart item" do
+        context "if its a new cart item" do
 
-    #         context "and the cart item does not have an accessory" do
+            context "and the cart item does not have an accessory" do
 
-    #             it "should build a new cart item" do
+                it "should build a new cart item" do
 
-    #             end
+                end
 
-    #             it "should update the cart item's quantity" do
+                it "should update the cart item's quantity" do
 
-    #             end
+                end
 
-    #             it "should update the cart item's weight" do
+                it "should update the cart item's weight" do
 
-    #             end
-    #         end
+                end
+            end
 
-    #         context "and the cart item has an accessory" do
+            context "and the cart item has an accessory" do
 
-    #             it "should build a new cart item" do
+                it "should build a new cart item" do
 
-    #             end
+                end
 
-    #             it "should build a new cart item accessory" do
+                it "should build a new cart item accessory" do
 
-    #             end
+                end
 
-    #             it "should update the cart item's quantity" do
+                it "should update the cart item's quantity" do
 
-    #             end
+                end
 
-    #             it "should update the cart item's weight" do
+                it "should update the cart item's weight" do
 
-    #             end
-    #         end
-    #     end
+                end
+            end
+        end
 
-    #     context "if its updating an existing cart item" do
+        context "if its updating an existing cart item" do
 
-    #         context "and the cart item does not have an accessory" do
+            context "and the cart item does not have an accessory" do
 
-    #             it "should update the cart item's quantity" do
+                it "should update the cart item's quantity" do
 
-    #             end
+                end
 
-    #             it "should update the cart item's weight" do
+                it "should update the cart item's weight" do
 
-    #             end
+                end
 
-    #         end
+            end
 
-    #         context "and the cart item has an accessory" do
+            context "and the cart item has an accessory" do
 
-    #             it "should update the cart item's quantity" do
+                it "should update the cart item's quantity" do
 
-    #             end
+                end
 
-    #             it "should update the cart item's weight, including the accessory weight" do
+                it "should update the cart item's weight, including the accessory weight" do
 
-    #             end
+                end
 
-    #             it "should update the associated cart item accessory quantity" do
+                it "should update the associated cart item accessory quantity" do
 
-    #             end
+                end
 
-    #         end
-    #     end
+            end
+        end
 
-    # end
+    end
 
     describe "When deleting a cart item" do
 
