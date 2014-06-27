3101da91f94987e23ecddd4162a5f07b8deca37c
Billy Dallimore <billy@sngtrkr.com>
2014-06-27 16:28:00 +0100

    Fixed bug with rogue tiers in the tier calculation method.

--- spec/lib/shipatron_4000_spec.rb
+++ spec/lib/shipatron_4000_spec.rb
@@ -7,6 +7,7 @@ describe Shipatron4000 do
     # thickness 12.34
     describe "Calculating the relevant tiers for an order" do
         let(:cart) { create(:tier_calculated_cart) }
+        Tier.destroy_all
         let!(:tier_1) { create(:tier, weight_start: BigDecimal.new("0"), weight_end: BigDecimal.new("26.75"), length_start: BigDecimal.new("0"), length_end: BigDecimal.new("103.23"), thickness_start: BigDecimal.new("0"), thickness_end: BigDecimal.new("22.71")) }
         let!(:tier_2) { create(:tier, weight_start: BigDecimal.new("0"), weight_end: BigDecimal.new("18.75"), length_start: BigDecimal.new("0"), length_end: BigDecimal.new("119.23"), thickness_start: BigDecimal.new("0"), thickness_end: BigDecimal.new("49.90")) }
         let!(:tier_3) { create(:tier, weight_start: BigDecimal.new("22.67"), weight_end: BigDecimal.new("46.75"), length_start: BigDecimal.new("66.82"), length_end: BigDecimal.new("201.45"), thickness_start: BigDecimal.new("12.33"), thickness_end: BigDecimal.new("52.62")) }
