66a16a5c621fb48df8990a22c15bde618305b165
Billy Dallimore <billy@sngtrkr.com>
2014-07-11 14:15:35 +0100

    House cleaning.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -160,10 +160,10 @@ module ApplicationHelper
         flash_array = []
         flash.each do |type, messages|
             if messages.is_a?(String)
-                flash_array << render(partial: 'shared/flash', locals: { :type => type, :message => messages })
+                flash_array << render(partial: 'shared/flash/admin', locals: { :type => type, :message => messages })
             else
                 messages.each do |m|
-                    flash_array << render(partial: 'shared/flash', locals: { :type => type, :message => m }) unless m.blank?
+                    flash_array << render(partial: 'shared/flash/admin', locals: { :type => type, :message => m }) unless m.blank?
                 end
             end
         end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,17 @@ Trado::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.assets.precompile += ['administration.js', 'administration.css','rails_admin/rails_admin.css', 'rails_admin/rails_admin.js', 'custom.css', 'css3-fallback.js', 'modernizr/modernizr.js', 'orders.js', 'products.js', 'typeahead.js/dist/typeahead.jquery.min.js']
+  config.assets.precompile += [ 'administration.js', 
+                                'administration.css',
+                                'rails_admin/rails_admin.css', 
+                                'rails_admin/rails_admin.js', 
+                                'custom.css', 
+                                'css3-fallback.js', 
+                                'modernizr/modernizr.js', 
+                                'orders.js', 
+                                'products.js', 
+                                'typeahead.js/dist/typeahead.jquery.min.js'
+                              ]
 
   # Generate digests for assets URLs
   config.assets.digest = true
