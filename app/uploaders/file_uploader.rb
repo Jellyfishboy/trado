7f0200ae36c564bab7b56268065b5e09f619b8a8
Tom Dallimore <tom.alan.dallimore@googlemail.com>
2014-09-19 14:50:42 +0800

    Removed unnecessary slash. TRAD-17

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -36,7 +36,7 @@ class FileUploader < CarrierWave::Uploader::Base
 
   #Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    ActionController::Base.helpers.asset_path("/assets/fallback/" + version_name.to_s + "/default.png")
+    ActionController::Base.helpers.asset_path("fallback/" + version_name.to_s + "/default.png")
   end
 
   # Process files as they are uploaded:
