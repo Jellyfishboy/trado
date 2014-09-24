7668c8a0f84dd86aeee6b85ac4f9ca887a56321f
Tom Dallimore <tom.alan.dallimore@googlemail.com>
2014-09-25 00:47:01 +0800

    Continued development on adjuting on the user experience for the checkout. TRAD-20 #time 1h User experience adjustments

--- app/helpers/delivery_service_price_helper.rb
+++ app/helpers/delivery_service_price_helper.rb
@@ -3,4 +3,8 @@ module DeliveryServicePriceHelper
     def dimension_range min, max
         [min, max].join(' - ')
     end
+
+    def cart_delivery_price cart_delivery_price
+        return cart_delivery_price.nil? ? 0 : cart_delivery_price.price
+    end
 end
