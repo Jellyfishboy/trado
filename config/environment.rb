bf37291967d1933d15e248001ba69c3ca30b008d
Tom Dallimore <tom.alan.dallimore@googlemail.com>
2014-10-06 01:59:13 +0800

    Added dependency injection back into the environment config - not sure this is the best way, must revise.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-
 # Initialize the rails application
 Trado::Application.initialize!
+require_dependency 'shipatron_4000'
+require_dependency 'mailatron_4000'
+require_dependency 'payatron_4000'
+require_dependency 'deep_struct'
+require_dependency 'store'
+require_dependency 'renderer'
+require_dependency 'dynamic_router'
 
 # begin
 #     ActiveRecord::Base.connection
