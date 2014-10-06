421aedfae971ce60e65647b7ad61ab7cf72d109c
Tom Dallimore <tom.alan.dallimore@googlemail.com>
2014-10-07 01:04:16 +0800

    Updated sitemap to facilitate the new pages functionality. TRAD-22 GR-4 #time Updated sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -7,16 +7,17 @@ SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
 
 SitemapGenerator::Sitemap.create do
 
-  add about_path, :lastmod => Time.now, :changefreq => 'monthly', :priority => 1
-  add contact_path, :lastmod => Time.now, :changefreq => 'monthly', :priority => 1
-
-  Category.find_each do |category|
-    add category_path(category), :lastmod => category.updated_at
-
-    category.products.find_each do |product|
-      add category_product_path(category, product), :lastmod => product.updated_at
+    Page.find_each do |page|
+        add send("#{page.slug.split('-').join('_')}_path"), :lastmod => Time.now, :changefreq => 'monthly', :priority => 1
     end
-  end
+
+    Category.find_each do |category|
+        add category_path(category), :lastmod => category.updated_at
+
+        category.products.find_each do |product|
+            add category_product_path(category, product), :lastmod => product.updated_at
+        end
+    end
 
 end
 SitemapGenerator::Sitemap.ping_search_engines
