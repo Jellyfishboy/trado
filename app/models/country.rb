22bf4e565a78337c511cd3fe8995196ef62d7de0
Tom Dallimore <tom.alan.dallimore@googlemail.com>
2014-10-17 01:53:27 +0800

    Reapplied default scope name ascending order to countries, along with spec.

--- app/models/country.rb
+++ app/models/country.rb
@@ -20,4 +20,6 @@ class Country < ActiveRecord::Base
   has_many :delivery_services,                          through: :destinations
 
   validates :name,                                      uniqueness: true, presence: true
+
+  default_scope order('name ASC')
 end
