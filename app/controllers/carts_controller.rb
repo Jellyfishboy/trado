45d2fcd54026d6e3c96b96ac45cf5fd8d996aabe
Tom Dallimore <me@tomdallimore.com>
2014-10-26 21:47:18 +0800

    Fixing delivery estimate bug. TRAD-20

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -45,9 +45,10 @@ class CartsController < ApplicationController
     end
 
     def purge_estimate
-        current_cart.estimate_delivery_id = nil
-        current_cart.estimate_country_name = nil
-        current_cart.save(validate: false)
+        @cart = current_cart
+        @cart.estimate_delivery_id = nil
+        @cart.estimate_country_name = nil
+        @cart.save(validate: false)
         render partial: theme_presenter.page_template_path('carts/delivery_service_prices/estimate/success'), format: [:js]
     end
 
