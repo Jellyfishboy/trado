a5a8ccd73e4fcb6e7ba8219205164c5bc8655fd6
Tom Dallimore <me@tomdallimore.com>
2014-12-29 22:12:23 +0700

    Removed webiste field from contact message form.

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -3,7 +3,7 @@ class ContactMessage
     include ActiveModel::Conversion
     extend ActiveModel::Naming
 
-    attr_accessor :name, :email, :website, :message
+    attr_accessor :name, :email, :message
 
     validates :name, :message,                                              presence: true
     validates :email,                                                       presence: { message: 'is required' }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
