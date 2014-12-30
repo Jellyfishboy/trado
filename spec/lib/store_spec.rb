63b0d7ae7eb08ac613b78ccaac902c0c4f2ec8a2
Tom Dallimore <me@tomdallimore.com>
2014-12-30 15:31:19 +0700

    Updated the store library class specs. TRAD-32 TRAD-37 TRAD-38

--- spec/lib/store_spec.rb
+++ spec/lib/store_spec.rb
@@ -8,14 +8,14 @@ describe Store do
         let!(:settings) { create(:store_setting) }
 
         it "should assign a store_settings object to a class variable and return" do
-            expect(Store::settings).to eq settings
+            expect(Store.settings).to eq settings
         end
     end
 
     describe "After modifying store wide settings" do
         let!(:settings) { create(:store_setting) }
         it "should set the store_settings class variable to nil" do
-            expect(Store::settings).to eq settings
+            expect(Store.settings).to eq settings
             Store::reset_settings
             expect(Store.class_variable_get(:@@store_settings)).to eq nil
         end
@@ -25,7 +25,7 @@ describe Store do
         let!(:settings) { create(:store_setting, tax_rate: "15.4") }
 
         it "should divide the tax_rate value by 100" do
-            expect(Store::tax_rate).to eq BigDecimal.new("0.154")
+            expect(Store.tax_rate).to eq BigDecimal.new("0.154")
         end
     end
 
@@ -34,14 +34,14 @@ describe Store do
         context "if its a positive value" do
 
             it "should return true" do
-                expect(Store::positive?(5)).to eq true
+                expect(Store.positive?(5)).to eq true
             end
         end
 
         context "if its a negative value" do
 
             it "should return false" do
-                expect(Store::positive?(-50)).to eq false
+                expect(Store.positive?(-50)).to eq false
             end
         end
     end
@@ -50,7 +50,7 @@ describe Store do
         let(:product) { create(:product, active: true) }
 
         it "should set the record as inactive" do
-            Store::inactivate!(product)
+            Store.inactivate!(product)
             expect(product.active).to eq false
         end
     end
@@ -60,7 +60,7 @@ describe Store do
         let!(:skus) { create_list(:sku, 3, product_id: product.id) }
 
         it "should set all the records as inactive" do
-            Store::inactivate_all!(product.skus)
+            Store.inactivate_all!(product.skus)
             expect(product.skus.pluck(:active)).to eq [false, false, false]
         end
     end
@@ -69,7 +69,7 @@ describe Store do
         let(:product) { create(:product) }
 
         it "should set the record as active" do
-            Store::activate!(product)
+            Store.activate!(product)
             expect(product.active).to eq true
         end
     end
@@ -78,7 +78,7 @@ describe Store do
         let(:variant_type) { create(:variant_type) }
 
         it "should return a string" do
-            expect(Store::class_name(variant_type)).to eq 'Variant Type'
+            expect(Store.class_name(variant_type)).to eq 'Variant Type'
         end
     end
 
@@ -88,12 +88,12 @@ describe Store do
             let!(:category) { create(:category) }
 
             it "should return a failed string message" do
-                expect(Store::last_record(category, Category.all.count)).to match_array([:error, 'Failed to delete category - you must have at least one.'])
+                expect(Store.last_record(category, Category.all.count)).to match_array([:error, 'Failed to delete category - you must have at least one.'])
             end
 
             it "should not delete the record" do
                 expect{
-                    Store::last_record(category, Category.all.count)
+                    Store.last_record(category, Category.all.count)
                 }.to change(Category, :count).by(0)
             end
         end
@@ -102,11 +102,11 @@ describe Store do
             let!(:category) { create_list(:category, 3) }
 
             it "should return a success string message" do
-                expect(Store::last_record(category.first, Category.all.count)).to match_array([:success, 'Category was successfully deleted.'])
+                expect(Store.last_record(category.first, Category.all.count)).to match_array([:success, 'Category was successfully deleted.'])
             end
             it "should delete the record" do
                 expect{
-                    Store::last_record(category.first, Category.all.count)
+                    Store.last_record(category.first, Category.all.count)
                 }.to change(Category, :count).by(-1)
             end
         end
@@ -116,6 +116,46 @@ describe Store do
 
         it "should parameterize it's slug attribute, including removing underscores" do
             expect(Store.parameterize_slug('haha *@look_at_me !here')).to eq 'haha-look-at-me-here'
+        end
+    end
+
+    describe 'When archiving a record' do
+        let!(:sku) { create(:sku, active: true) }
+
+
+        context "if the record has associated carts" do
+            before(:each) do
+                create(:cart_item, sku_id: sku.id)
+            end
+
+            it "should delete the assocaited cart item records" do
+                expect{
+                    Store.active_archive(CartItem, :sku_id, sku)
+                }.to change(CartItem, :count).by(-1)
+            end 
+        end
+
+        context "if the record has assocaited orders" do
+            before(:each) do
+                create(:order_item, sku_id: sku.id)
+            end
+
+            it "should change the record's active attribute to false" do
+                expect{
+                    Store.active_archive(CartItem, :sku_id, sku)
+                }.to change{
+                    sku.active
+                }.from(true).to(false)
+            end
+        end
+
+        context "if the record does not have associated orders" do
+
+            it "should destroy the record" do
+                expect{
+                    Store.active_archive(CartItem, :sku_id, sku)
+                }.to change(Sku, :count).by(-1)
+            end
         end
     end
 end
