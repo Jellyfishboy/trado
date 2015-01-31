de3ad561686b2241807bcc8a161843a8e0992823
Tom Dallimore <me@tomdallimore.com>
2015-01-31 15:52:08 +0700

    Fixing bugs in test suite.

--- spec/helpers/stock_helper_spec.rb
+++ spec/helpers/stock_helper_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe StockHelper do
 
     describe '#latest_stock_adjustment' do
-        let!(:sku) { create(:sku, active: true) }
-        let!(:stock_adjustment) { create(:stock_adjustment, sku_id: sku.id) }
+        let!(:sku) { create(:skip_after_stock_adjustment_sku, active: true) }
+        let!(:stock_adjustment) { create(:stock_adjustment, sku_id: sku.id, created_at: Date.yesterday) }
         
         context "if the stock adjustment is equal to the latest SKU stock adjustment record" do
 
