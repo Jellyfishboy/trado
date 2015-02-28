351a93d2582fd8cb25e3c495a4345c95ed00887b
Tom Dallimore <me@tomdallimore.com>
2015-02-28 18:23:51 +0700

    Fixed the scheduled configuration so rbenv works alongside cronjobs. TRAD-47

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,34 +1,19 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, "/home/gimsonrobotics/current/log/schedule.log"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+job_type :rbenv_rake, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
+                         cd :path && RAILS_ENV=production bundle exec rake :task --silent :output }
+job_type :rbenv_runner, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
+                         cd :path && RAILS_ENV=production bundle exec rails runner :task --silent :output }
 
-# Learn more: http://github.com/javan/whenever
-set :output, "/var/log/your_app_name/schedule.log"
-
-every 1.day, :at => '4:10am' do
-    runner "Cart.clear_carts"
+every 1.day, :at => '4:00am' do
+    rbenv_runner "Cart.clear_carts"
 end
 
 every 1.day, :at => '5:00 am' do
-  rake "-s sitemap:refresh"
+    rbenv_rake "-s sitemap:refresh"
 end
 
 every 1.day, :at => '9:00 am' do
-    runner "Mailatron4000::Stock.notify"
-end 
+    rbenv_runner "Mailatron4000::Stock.notify"
+end
 
