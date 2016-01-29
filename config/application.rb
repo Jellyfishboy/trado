79148027b5814eeca5996641c9de628842853f67
Tom Dallimore <me@tomdallimore.com>
2016-01-29 13:52:15 +0700

    Removed redundant config for whitelist attributes and fixed travis build.

--- config/application.rb
+++ config/application.rb
@@ -60,12 +60,6 @@ module Trado
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
-
     # Stops connecting to database while precompiling assets on deployment with capistrano
     config.assets.initialize_on_precompile = true
 
