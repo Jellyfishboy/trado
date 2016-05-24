de9250278796494f0995b721be6d2c68904e6f07
Tom Dallimore <me@tomdallimore.com>
2016-05-24 19:27:43 +0700

    Reset popular countries first.

--- app/jobs/regenerate_popular_countries_job.rb
+++ app/jobs/regenerate_popular_countries_job.rb
@@ -2,11 +2,16 @@ class RegeneratePopularCountriesJob < ActiveJob::Base
     queue_as :default
 
     def perform *args
+        reset_popular_countries
         set_countries
         @countries.map{|c| c.update_column(:popular, true) }
     end
 
     private
+
+    def reset_popular_countries
+        Country.all.update_all(popular: false)
+    end
 
     def set_countries
         @countries = Country.joins(:orders).group("countries.id").order("count(countries.id) DESC").first(10)
