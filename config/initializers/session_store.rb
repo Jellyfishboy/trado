45712395cd18fbb985fbf60471a73b2473a17109
Tom Dallimore <me@tomdallimore.com>
2016-07-21 17:40:20 +0700

    Changed session store to cache store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Trado::Application.config.session_store :cookie_store, key: '_trado_session'
+# Trado::Application.config.session_store :cookie_store, key: '_trado_session'
+Trado::Application.config.session_store ActionDispatch::Session::CacheStore
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
