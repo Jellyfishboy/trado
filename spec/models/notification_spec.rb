058495329a35b6b252ec1b0b1d70d768dba5525e
David Henner <drhenner@yahoo.com>
2016-08-19 20:10:30 -0700

    use let in rspec

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -28,25 +28,26 @@ describe Notification do
     it { expect(create(:sku_notification)).to validate_uniqueness_of(:email).scoped_to([:notifiable_id, :notifiable_type, :sent_at]).with_message('notification has already been created.') }
 
     context 'skus and users can be associated to a notification' do
-      let(:duplicate_id) { 1000000009 }
-      let!(:sku)  { FactoryGirl.create(:sku, id: duplicate_id) }
-      let!(:user) { FactoryGirl.create(:user, id: duplicate_id) }
+      let(:duplicate_id)      { 1000000009 }
+      let(:sku)               { FactoryGirl.create(:sku, id: duplicate_id) }
+      let(:user)              { FactoryGirl.create(:user, id: duplicate_id) }
+      let(:sku_notification)  { FactoryGirl.create(:sku_notification, email: 'qwerty21@gmail.com', notifiable: sku) }
+      let(:user_notification) { FactoryGirl.build(:user_notification, email: 'qwerty21@gmail.com', notifiable: user) }
 
       it 'should allow different models to be associated' do
-        sku_notification = create(:sku_notification, email: 'qwerty21@gmail.com', notifiable: sku)
         expect(sku_notification.valid?).to be true
-        notification = build(:user_notification, email: 'qwerty21@gmail.com', notifiable: user)
-        expect(notification.valid?).to be true
-        expect(notification.save).to be true
+        expect(user_notification.valid?).to be true
+        expect(user_notification.save).to be true
       end
     end
 
     context 'skus and users can be associated to a notification' do
-      let(:user) { FactoryGirl.create(:user) }
+      let(:user)              { FactoryGirl.create(:user) }
+      let(:notification)      { FactoryGirl.build(:user_notification, email: 'qwerty22@gmail.com', notifiable: user) }
+      let(:user_notification) { FactoryGirl.create(:user_notification, email: 'qwerty22@gmail.com', notifiable: user, sent_at: 2.years.ago, created_at: 3.years.ago) }
 
       it 'should allow a second association if the first was sent years ago' do
-        expect(create(:user_notification, email: 'qwerty22@gmail.com', notifiable: user, sent_at: 2.years.ago, created_at: 3.years.ago).valid?).to be true
-        notification = build(:user_notification, email: 'qwerty22@gmail.com', notifiable: user)
+        expect(user_notification.valid?).to be true
         expect(notification.valid?).to be true
         expect(notification.save).to be true
       end
