74648ce1433edc2490170b4351e023eae444be2f
Tom Dallimore <me@tomdallimore.com>
2016-08-23 02:41:19 +0700

    More work on building up the stock adjustment controller for mass creation.

--- app/controllers/admin/products/stock_adjustments_controller.rb
+++ app/controllers/admin/products/stock_adjustments_controller.rb
@@ -8,6 +8,12 @@ class Admin::Products::StockAdjustmentsController < ApplicationController
 
     def create
         set_skus
+        if create_stock_adjustments
+            flash_message :success, 'Stock adjustments was successfully created.'
+            redirect_to admin_products_stock_index_url
+        else
+            render :new
+        end
     end
 
     private
@@ -18,5 +24,9 @@ class Admin::Products::StockAdjustmentsController < ApplicationController
 
     def new_stock_adjustment
         @stock_adjustment ||= StockAdjustment.new
+    end
+
+    def create_stock_adjustments
+        StockAdjustment.create(params[:stock_adjustment])
     end
 end
