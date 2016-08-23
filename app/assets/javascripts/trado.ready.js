70b852c00fa0195985beca2414bae83c778860e9
Tom Dallimore <me@tomdallimore.com>
2016-08-23 14:36:47 +0700

    Building out multi field functionality.

--- app/assets/javascripts/trado.ready.js
+++ app/assets/javascripts/trado.ready.js
@@ -42,6 +42,17 @@ ready = function()
     trado.admin.newVariants();
     trado.admin.resetVariants();
     trado.admin.amendVariants();
+
+    function remove_fields(link) {
+        $(link).prev("input[type=hidden]").val("1");
+        $(link).closest(".fields").hide();
+    }
+
+    function add_fields(link, association, content) {
+        var new_id = new Date().getTime();
+        var regexp = new RegExp("new_" + association, "g")
+        $(link).parent().before(content.replace(regexp, new_id));
+    }
 };
 
 $(document).ready(ready);
