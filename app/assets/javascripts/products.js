8c663028c0196c8523ca2390eba19f649ee72333
Tom Dallimore <me@tomdallimore.com>
2016-09-04 19:25:13 +0700

    Fixed the interval loop bug.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,31 +1,29 @@
 //= require jquery.remotipart
-$(document).ready(function()
-{
-    function productAutosave()
-    {
-        var $form = $('.edit_product'),
-        productId = $form.attr('data-product-id');
-        $.ajax(
-        {
-            url: '/admin/products/' + productId + '/autosave',
-            type: 'PATCH',
-            data: $form.serialize(),
-            dataType: 'json',
-            success: function (data)
-            {
-                $('#autosave-message').show();
-                $('#autosave-message').html("<i class='icon-checkmark icon-green'></i> All Changes Saved");
-            },
-            error: function(xhr, evt, status)
-            {
-                $('#autosave-message').show();
-                $('#autosave-message').html("<i class='icon-close icon-red'></i> Validation Failed");
-            }
-        });
-    };
 
-    setInterval(function()
+function productAutosave()
+{
+    var $form = $('.edit_product'),
+    productId = $form.attr('data-product-id');
+    $.ajax(
     {
-        productAutosave()
-    }, 20000);
-});
+        url: '/admin/products/' + productId + '/autosave',
+        type: 'PATCH',
+        data: $form.serialize(),
+        dataType: 'json',
+        success: function (data)
+        {
+            $('#autosave-message').show();
+            $('#autosave-message').html("<i class='icon-checkmark icon-green'></i> All Changes Saved");
+        },
+        error: function(xhr, evt, status)
+        {
+            $('#autosave-message').show();
+            $('#autosave-message').html("<i class='icon-close icon-red'></i> Validation Failed");
+        }
+    });
+};
+
+window.setInterval(function()
+{
+    productAutosave()
+}, 20000);
