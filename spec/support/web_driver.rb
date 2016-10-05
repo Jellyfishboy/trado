0656cfad5bf49673cc60743f602c6e3471e60bbd
Tom Dallimore <me@tomdallimore.com>
2016-10-05 19:00:19 +0700

    Updated web driver config.

--- spec/support/web_driver.rb
+++ spec/support/web_driver.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
-  config.before :suite, js: :true do
+  config.before :each, js: :true do
       require 'capybara/poltergeist'
       Capybara.register_driver :poltergeist do |app|
         Capybara::Poltergeist::Driver.new(app, {
