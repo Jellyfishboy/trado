00c40a3584677d3e1e5cdd21f288f1e6ea377707
Tom Dallimore <me@tomdallimore.com>
2016-11-16 18:58:24 +0700

    Fixing redis url and assigning worker dyno in heroku deploy config.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
+redis_url = Rails.env.production? ? ENV["REDIS_URL"] : 'redis://localhost:6379/12'
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/12' }
+    config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/12' }
+    config.redis = { url: redis_url }
 end
