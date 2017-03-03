eaeb464e74e948f33f226ba3057a85ea432b37e9
Tom Dallimore <me@tomdallimore.com>
2017-03-03 14:48:29 +0700

    Fixed bug with product scope.

--- app/models/product.rb
+++ app/models/product.rb
@@ -73,7 +73,7 @@ class Product < ActiveRecord::Base
   
   default_scope { order(weighting: :desc) }
 
-  scope :published_or_archived,                               -> { where(status: [0,2]) } 
+  scope :published_or_archived,                               -> { where(status: [1,2]) } 
   scope :search,                                              ->(query, page, per_page_count, limit_count) { joins(:tags).where("lower(products.name) LIKE :search OR lower(products.sku) LIKE :search OR lower(tags.name) LIKE :search", search: "%#{query}%").uniq.limit(limit_count).page(page).per(per_page_count) }
 
   enum status: [:draft, :published, :archived]
