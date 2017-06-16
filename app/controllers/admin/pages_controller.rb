b33431919620ec513c91c6de49b16036a8397290
Tom Dallimore <me@tomdallimore.com>
2017-06-16 22:08:27 +0700

    Added admin controller translations.

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -15,7 +15,7 @@ class Admin::PagesController < Admin::AdminBaseController
     list_template_types
     params[:page][:slug] = Store.parameterize_slug(params[:page][:slug])
     if @page.update(params[:page])
-      flash_message :success, 'Page was successfully updated.'
+      flash_message :success, t('controllers.admin.pages.update.valid')
       redirect_to admin_pages_url
     else
       render :edit
