9527563a19c1b8f8b31cb4a228a34f00ef9929bd
Malachai Frazier The Great <malachaifrazier@gmail.com>
2017-12-22 15:39:48 -0500

    HasAttachments attachment_count validation on create added

--- app/models/concerns/has_attachments.rb
+++ app/models/concerns/has_attachments.rb
@@ -1,17 +1,17 @@
 module HasAttachments
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do
-        validate :attachment_count,                                 :if => :published?
-    end
+  included do
+    validate :attachment_count, if: :published?, on: :create
+  end
 
-    # Calculate if a product has at least one associated attachment
-    # If no associated attachments exist, return an error
-    #
-    def attachment_count
-        if self.attachments.map(&:default_record).count == 0
-            errors.add(:product, " must have at least one attachment.")
-            return false
-        end
+  # Calculate if a product has at least one associated attachment
+  # If no associated attachments exist, return an error
+  #
+  def attachment_count
+    if self.attachments.map(&:default_record).count == 0
+      errors.add(:product, " must have at least one attachment.")
+      return false
     end
+  end
 end
