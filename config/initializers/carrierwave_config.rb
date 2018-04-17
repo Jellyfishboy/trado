2a40931026054b899f1c5653e7f99db6f68cfbe5
Tom Dallimore <me@tomdallimore.com>
2018-04-17 23:51:48 +0700

    Disabled s3 and asset sync.

--- config/initializers/carrierwave_config.rb
+++ config/initializers/carrierwave_config.rb
@@ -1,21 +1,4 @@
 CarrierWave.configure do |config|
 
-    if Rails.env.production?
-        config.storage :fog
-        config.fog_credentials = {
-            :provider => 'AWS',
-            :aws_access_key_id => ENV['AWS_S3_ID'],
-            :aws_secret_access_key => ENV['AWS_S3_KEY'],
-            :region => ENV['AWS_S3_REGION'],
-        }
-        config.fog_directory = ENV['AWS_S3_BUCKET']
-        config.asset_host = ENV['CARRIERWAVE_URL']
-        config.fog_public = true
-        config.fog_attributes = {
-          'Cache-Control' => 'max-age=315576000',
-          'x-amz-storage-class' => 'REDUCED_REDUNDANCY'
-        }
-    else
-        config.storage = :file
-    end
+    config.storage = :file
 end
